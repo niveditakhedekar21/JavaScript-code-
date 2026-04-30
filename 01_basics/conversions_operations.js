e882f835332cbfc0e797b66bdebbad51d7e6aa3b
Nivedita Khedekar <niveditakhedekar21@gmail.com>
2026-04-30 17:17:40 +0000

    conversions and operations

--- 01_basics/conversions_operations.js
+++ 01_basics/conversions_operations.js
@@ -1,3 +1,5 @@
+//******************************CONVERSIONS********************************** */
+
 /*
 let score=true; //when we have score value is 33abc then output will be NAN means not a number
 //if we change score value in boolean then output will be shown as 0 and 1
@@ -28,8 +30,44 @@ console.log(booleanIsLoggedIn);
 //nivedita=>true
 */
 
-
+/*
 let someNumber=33;
 let stringNumber= String (someNumber);
 console.log(  stringNumber);
 console.log( typeof stringNumber);
+*/
+
+
+
+//******************************OPERATIONS****************************************
+
+let value=3;
+let negvalue= -value;
+//console.log(negvalue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/3);
+//console.log(2%3);
+
+//let str1="hello"
+//let str2=" nivedita"
+//let str3=str1+ str2
+//console.log(str3)
+//
+//console.log("1" +2)
+//console.log(1+"2")
+//console.log("1"+ 2+ 2)
+//console.log(1+ 2+ "2")
+//console.log (true)
+//console.log (+true)
+//console.log (true+)  //it gives error coz this operation cannoti be performed
+//console.log(+"")
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gamecounter=100
+gamecounter++;//prefix and postfix mdn document
+console.log(gamecounter)
